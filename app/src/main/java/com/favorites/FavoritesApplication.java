c8f8c43a278ab9c53f508cbde61473b64f4d671f
18667012791@163.com <xg_415>
2019-05-15 15:56:06 +0800

    主要服务的端口号

--- app/src/main/java/com/favorites/FavoritesApplication.java
+++ app/src/main/java/com/favorites/FavoritesApplication.java
@@ -2,19 +2,23 @@ package com.favorites;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class FavoritesApplication extends SpringBootServletInitializer {
-	
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(FavoritesApplication.class);
-    }
+@RestController
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class FavoritesApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(FavoritesApplication.class, args);
+    }
+
+    @ResponseBody
+    @GetMapping("test")
+    public String test() {
+	    return "hello world";
     }
 
 }
